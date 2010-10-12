95b56ae2ea7f8b82919522d9661b49023d7ed6a1
Roman <roman.simecek@screenconcept.ch>
2010-10-12 18:11:33 +0200

    Initial commit after creating the rails 3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interactivehomedevicecontroller::Application.config.secret_token = '7c192b1ea83afb2db3f745ea18fd5cbbeb71c5e90ae31e59258aad0ccaf6bf855d22e7ade44860edd0f03a3d0880105679b4decec9a86ead500210a96bf33a3c'
