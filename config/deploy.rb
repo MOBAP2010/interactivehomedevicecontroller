ed88756b54421dbe6e3d83297b10c1dd86f92945
Roman <roman.simecek@screenconcept.ch>
2010-11-27 00:11:03 +0100

    Enabled memcache.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,12 +3,17 @@ require 'bundler/capistrano'
 ssh_options[:forward_agent] = true
 set :repository, "git@github.com:MOBAP2010/interactivehomedevicecontroller.git"  # Your clone URL
 set :scm, "git"
+set :branch, "master"
 set :user, "raskhadafi"  # The server's user for deploys
 set :deploy_via, :remote_cache
 set :deploy_to, '/srv/good2go.ch/mobap/'
+set :git_enable_submodules, 1
 
 set :application, "interactivehomedevicecontroller"
 set :use_sudo, false
+
+set :rvm_ruby_string, "1.9.2@rails3"
+set :rvm_type, :system
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
@@ -20,6 +25,9 @@ role :db,  "mobap.good2go.ch", :primary => true
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
+
+require "bundler/capistrano"
+after "bundle:install", "deploy:migrate"
 
 namespace :deploy do
   task :start do ; end
